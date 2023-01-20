29fa460f3b8bcc2f814790f227bdcf9ffd1b17ca
Cara Lowe <2602907l@student.gla.ac.uk>
2023-01-20 17:53:06 +0000

    syntax

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about  page <a href='/rango/'>Index</a>")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
