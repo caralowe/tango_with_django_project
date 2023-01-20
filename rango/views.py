1240dc962872903c3e010dd1d2718410d688521f
Cara Lowe <2602907l@student.gla.ac.uk>
2023-01-20 16:52:30 +0000

    first hyperlink

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+def about():
+    return HttpResponse("Rango says here is the " <a href='/rango/about/'>about</a> " page" )
