34c5ad3f2dc3db769110bd33326706cb65a62eb3
Cara Lowe <2602907l@student.gla.ac.uk>
2023-01-29 20:24:00 +0000

    5.7 finished

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -8,12 +8,11 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url' : 'http://docs.python.org/3/tutorial/'},
-        {'title' : 'How to Think like a Computer Scientist',
-        'url' : 'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://docs.python.org/3/tutorial/'},
+        {'title':'How to Think like a Computer Scientist',
+        'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'}
-    ]
+        'url':'http://www.korokithakis.net/tutorials/python/'} ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
@@ -21,29 +20,27 @@ def populate():
         {'title':'Django Rocks',
         'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'}
-    ]
+        'url':'http://www.tangowithdjango.com/'} ]
 
     other_pages = [
         {'title':'Bottle',
-        'url':'https://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'}
-    ]
+        'url':'http://flask.pocoo.org'} ]
 
     cats = {'Python': {'pages': python_pages},
-            'Django':{'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+        'Django': {'pages': django_pages},
+        'Other Frameworks': {'pages': other_pages} }
+
 
     for cat, cat_data in cats.items():
-        c = add_cat(cats)
+        c = add_cat(cat)
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
-    for c in Category.object.all():
+    for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
-
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
